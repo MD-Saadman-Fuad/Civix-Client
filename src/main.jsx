f5e03a97fe723f689e9f4b7a82c907e8a6f82c4c
MD-Saadman-Fuad <md.saadman.fuad@gmail.com>
2025-11-12 16:18:33 +0600

    Login, Register Page, PrivateRoute, authContext, authprovider Added

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './Layout/Home.jsx'
+import Home from './Layout/Home.jsx';
+import Login from './pages/Login.jsx';
+import AuthProvider from './Context/AuthProvider.jsx';
+import Register from './pages/Register.jsx';
 
 const router = createBrowserRouter([
   {
@@ -18,12 +21,18 @@ const router = createBrowserRouter([
         path: 'login',
         element: <Login />,
       },
+      {
+        path: 'register',
+        element: <Register />,
+      }
     ],
   },
 ]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </StrictMode>,
 )
